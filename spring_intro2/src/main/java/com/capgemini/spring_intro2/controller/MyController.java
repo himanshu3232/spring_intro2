8531f1f98f82e03b72cf9f14881f6a97a7353c0b
himanshu3232 <himanshu2000ranjan@gmail.com>
2025-02-22 03:29:10 +0530

    fix

--- spring_intro2/src/main/java/com/capgemini/spring_intro2/controller/MyController.java
+++ spring_intro2/src/main/java/com/capgemini/spring_intro2/controller/MyController.java
@@ -2,6 +2,7 @@ package com.capgemini.spring_intro2.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
