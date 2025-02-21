c84d9bd86fe0be70b67942097e89aa31d4052f9c
himanshu3232 <himanshu2000ranjan@gmail.com>
2025-02-22 03:20:20 +0530

    [Himanshu Ranjan] init

---
+++ spring_intro2/src/test/java/com/capgemini/spring_intro2/SpringIntro2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.spring_intro2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntro2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
